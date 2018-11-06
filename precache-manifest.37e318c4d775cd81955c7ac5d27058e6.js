c5ff320eb88c923ef65afd279fd5ba7913b49f3f
모바일-강종혁 <kangjh@trebit.com>
2018-11-06 14:16:32 +0900

    Updates

---
+++ precache-manifest.37e318c4d775cd81955c7ac5d27058e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "f2825fddef990461cd4e",
+    "url": "/movie_app/static/js/main.f2825fdd.chunk.js"
+  },
+  {
+    "revision": "392d6e18dd1fb16cb5e2",
+    "url": "/movie_app/static/js/1.392d6e18.chunk.js"
+  },
+  {
+    "revision": "f2825fddef990461cd4e",
+    "url": "/movie_app/static/css/main.0fa516c6.chunk.css"
+  },
+  {
+    "revision": "aebc0590bf59205ab202bc7a7a16428b",
+    "url": "/movie_app/index.html"
+  }
+];
